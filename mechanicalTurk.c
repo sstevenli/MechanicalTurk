7108710e05abb635312a9af6244dc86a50a71125
Hayden <riotoushayden@live.com.au>
2015-05-21 23:00:39 +1000

    added legal iteration function

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 #include "Game.h"
 #include "mechanicalTurk.h"
@@ -13,12 +14,12 @@
 #define PATH_INITIAL "RLRL"
 #define PATH_SEQUENCE "RLLLLLRLLLLBLLLLBLLLLBLLLLBLLLLBLLLBLLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLL"
 
-static void nextPath( path previousPath, int index );
+static void nextPath( path previousPath);
+static action iterateLegal (int actionCode, Game g);
 
 action decideAction (Game g) {
 
     action nextAction;
-
     /*Order of actions:
      * Go8 
      * Campuses
@@ -27,39 +28,71 @@ action decideAction (Game g) {
      * (trading)
      */
     int myID = getWhoseTurn(g);
-    int THDs = getStuents (g, myID, STUDENT_THD);
-    int BPSs = getStuents (g, myID, STUDENT_BPS);
-    int BQNs = getStuents (g, myID, STUDENT_BQN);
-    int MJs = getStuents (g, myID, STUDENT_MJ);
-    int MTVs = getStuents (g, myID, STUDENT_MTV);
-    int MMONEYs = getStuents (g, myID, STUDENT_MMONEY);
+    int THDs = getStudents (g, myID, STUDENT_THD);
+    int BPSs = getStudents (g, myID, STUDENT_BPS);
+    int BQNs = getStudents (g, myID, STUDENT_BQN);
+    int MJs = getStudents (g, myID, STUDENT_MJ);
+    int MTVs = getStudents (g, myID, STUDENT_MTV);
+    int MMONEYs = getStudents (g, myID, STUDENT_MMONEY);
 
-    if (MJs >= 2 && MMONEYs >=3) {
+    int Go8Legal = TRUE;
+    int CampusLegal = TRUE;
+    int ArcLegal = TRUE;
+
+    if (MJs >= 2 && MMONEYs >=3 && Go8Legal == TRUE) {
         //build Go8
-    } else if (BPSs >=1 && BQNs >=1 && MJs >=1 && MTVs >=1) {
+        action tempAction = iterateLegal(BUILD_GO8, g);
+        if (tempAction.actionCode = PASS) {
+            Go8Legal = FALSE;
+        }
+    } 
+    if (BPSs >=1 && BQNs >=1 && MJs >=1 && MTVs >=1 && CampusLegal == TRUE) {
         //build campus
-    } else if (BQNs >=1 && BPSs >=1) {
+        action tempAction = iterateLegal(BUILD_CAMPUS, g);
+        if (tempAction.actionCode = PASS) {
+            CampusLegal = FALSE;
+        }
+    } 
+    if (BQNs >=1 && BPSs >=1 && ArcLegal == TRUE) {
         //build arc
-    } else if (MJs >=1 && MTVs >=1 && MMONEYs>=1) {
+        action tempAction = iterateLegal(OBTAIN_ARC, g);
+        if (tempAction.actionCode = PASS) {
+            ArcLegal = FALSE;
+        }
+    } 
+    if (MJs >=1 && MTVs >=1 && MMONEYs>=1) {
         //make patent
-    } else {
-        //trade
+        action tempAction = iterateLegal(START_SPINOFF, g);
     }
-/*
->>>>>>> actionOrder
-    nextAction.actionCode = START_SPINOFF;
-
-    if ( !isLegalAction( g, nextAction ) ) {
-        nextAction.actionCode = PASS;
-    }
-*/
-    return nextAction;
+    //trade
+    //tempAction = iterateLegal(RETRAIN STUDENTS, g);
 }
 
+static action iterateLegal (int actionCode, Game g) {
+    path tempPath = PATH_INITIAL;
+    action tempAction;
+    tempAction.actionCode = actionCode;
+    strcpy(tempAction.destination, tempPath);
+    //tempAction.disciplineFrom = from;
+    //tempAction.disciplineTo = to;
+    int legal = FALSE;
+
+    while (strlen(tempPath) <= strlen(PATH_INITIAL) + strlen(PATH_SEQUENCE) && legal == FALSE) {
+        legal = isLegalAction(g, tempAction);
+        if (legal == FALSE) {
+            nextPath(tempPath);
+            strcpy(tempAction.destination, tempPath);
+        }
+    }
+    if (legal == FALSE) {
+       tempAction.actionCode = 0; 
+    }
+    return tempAction;
+}
 // index is the index of PATH_SEQUENCE
 // i.e. the current iteration of the pathing loop
-static void nextPath( path previousPath, int index ) {
-
+static void nextPath(path previousPath) {
+    int index = strlen(previousPath);
     path pathSequence = PATH_SEQUENCE;
     previousPath[index + 4]      = pathSequence[index];
     previousPath[index + 4 + 1] = '\0';
