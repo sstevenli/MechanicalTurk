48a335e6da95bcebcab902038a3f44a5b13273e2
Gyross <ar85ar@gmail.com>
2015-05-21 19:30:23 +1000

    Make mechanicalTurk.c mrPass

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -1,7 +1,25 @@
-#include "Game.h"
+/*
+ * Mr Pass.  Brain the size of yor mom!
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "game.h"
 #include "mechanicalTurk.h"
+
 action decideAction (Game g) {
-  action a;
-  a.actionCode = PASS;
-  return a;
+
+    action nextAction;
+
+    nextAction.actionCode = START_SPINOFF;
+
+    if ( !isLegalAction( g, nextAction ) ) {
+        nextAction.actionCode = PASS;
+    }
+
+    return nextAction;
+
 }
