e824525d9c70460abba641e8090091782b6f37bf
Gyross <ar85ar@gmail.com>
2015-05-21 21:41:15 +1000

    Implement nextPath()

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -13,11 +13,11 @@
 #define PATH_INITIAL "RLRL"
 #define PATH_SEQUENCE "RLLLLLRLLLLBLLLLBLLLLBLLLLBLLLLBLLLBLLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLLLBLLLBLLL"
 
+void nextPath( path previousPath, int index );
+
 action decideAction (Game g) {
 
     action nextAction;
-
-    path fullPath = 
 
     nextAction.actionCode = START_SPINOFF;
 
@@ -26,4 +26,14 @@ action decideAction (Game g) {
     }
 
     return nextAction;
+}
+
+// index is the index of PATH_SEQUENCE
+// i.e. the current iteration of the pathing loop
+void nextPath( path previousPath, int index ) {
+
+    path pathSequence = PATH_SEQUENCE;
+    (previousPath*)[index + 4]      = pathSequence[index];
+    (previousPath*)[index + 4 + 1] = '\0';
+
 }
